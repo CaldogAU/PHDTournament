cfefa848623fb94297a5bda98348bc36f03a9cbe
CaldogAU <callumh77@gmail.com>
2026-07-09 23:36:21 +1000

    default logo and banner

--- js/state.js
+++ js/state.js
@@ -10,8 +10,8 @@ const PHDTournament = {
     tournament: {
       name: "PHDlympics",
       description: "",
-      logoUrl: "",
-      bannerUrl: "",
+      logoUrl: "https://media.licdn.com/dms/image/v2/D4D0BAQEMKjVjgICdBQ/company-logo_200_200/company-logo_200_200/0/1708434484760/phd__logo?e=2147483647&v=beta&t=3V6CvaNB9bLOL84Ecc_OARY-vVa-WfTbPwu8_ExwIb8",
+      bannerUrl: "https://thumbs.dreamstime.com/b/kangaroo-funny-big-animal-marsupial-mammal-endemic-to-australia-rest-red-sand-ecotourism-concept-147824790.jpg",
       accentColour: "#6d5dfc",
       settings: {
         winPoints: 3,
